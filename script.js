8d4973d095a95fdcb6b26d38378d8f8c1fb09890
blackoutcoder <romka19841201@gmail.com>
2022-01-25 18:11:08 +0200

    main

---
+++ script.js
@@ -0,0 +1,59 @@
+//document.write("<br>Hello world");
+newFunction();
+
+function newFunction() {
+    document.write("<br>Hello world<br>");
+}
+
+            //peremennije
+//var number = 21; //Переменная может быть чем угодно (цифра, строка, условие)
+//var string; 
+//string = "Hello world";
+//var floatNumber = 1.23;
+//var isTrue;
+//isTrue = true;
+//document.write("Variable number =" + number + "<br>" + string + "<br>" + floatNumber + "<br>" + isTrue);
+
+            // Aritmethical functions
+//var x = 5;
+//var y;
+//var result;
+//y = 12;
+//result = x - y; //result = x * y, result = x / y
+//document.write(result);
+//result = result + x; // or result += x; 
+//document.write("<br>" + result); 
+//result ++  dobavliajet 1;
+//result --  otnomaet 1;
+//document.write(x + "+" + y + "=" + (x + y));
+
+//var str_1 = "12";
+//var str_2 = Number("24");
+//document.write(Number(str_1) + str_2);
+
+//var bool;
+//var z = 6;
+//var d = 7;
+//bool = z < d; // >, <, =, <=, >=.
+//document.write(bool);
+
+            //ostatok pri delenii
+//document.write(10 + "%" + 7 + "=" + (10%7)); // ostatok pri delenii 3;
+            
+
+            //Uslovnije operatori 
+//var x = 4;
+//var y = 5;
+//if (x > y) // == - sravnenije, != ne ravno
+//    document.write("x < y");
+//else if (x == y)
+//    document.write("x==y");    
+//else 
+//    document.write("x < y");   
+
+var result = 5;
+switch(result) {
+    
+}   
+
+            
